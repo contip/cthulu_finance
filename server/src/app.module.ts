0b91048a4ffff9ca1c7cfca1a93f0690e03c9ef7
Pete <petertconti@gmail.com>
2020-07-18 02:39:15 -0400

    begin working on login module

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -5,10 +5,12 @@ import { LookupController } from './lookup/lookup.controller';
 import { DatabaseModule } from './database/user.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { RegisterModule } from './register/register.module';
+import { LoginController } from './login/login.controller';
+import { LoginModule } from './login/login.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(), HttpModule, DatabaseModule, RegisterModule],
-  controllers: [LookupController],
+  imports: [TypeOrmModule.forRoot(), HttpModule, DatabaseModule, RegisterModule, LoginModule],
+  controllers: [LookupController, LoginController],
   providers: [AppService, LookupService]
 })
 export class AppModule {}
