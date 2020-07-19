9c0b409b77e1dc7790278acd24e237df431a5cb6
contip <contip@oregonstate.edu>
2020-07-19 02:10:58 -0400

    assign JWT upon successful login

--- server/src/auth/auth.module.ts
+++ server/src/auth/auth.module.ts
@@ -4,9 +4,18 @@ import { DatabaseModule } from '../database/user.module';
 import { PassportModule } from '@nestjs/passport';
 import { LocalStrategy } from './local.strategy';
 import { AuthController } from './auth.controller';
+import { JwtModule } from '@nestjs/jwt';
+import { jwtConstants } from './constants';
 
 @Module({
-  imports: [DatabaseModule, PassportModule],
+  imports: [
+    DatabaseModule, 
+    PassportModule, 
+    JwtModule.register({
+      secret: jwtConstants.secret,
+      signOptions: { expiresIn: '60s' },
+    }),
+  ],
   providers: [AuthService, LocalStrategy],
   controllers: [AuthController]
 })
