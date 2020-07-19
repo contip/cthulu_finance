e7b47b9331e14d64932e3c80504ff4b3a3c2700d
contip <contip@oregonstate.edu>
2020-07-19 02:26:25 -0400

    add basic route requiring JWT for access

--- server/src/auth/auth.controller.ts
+++ server/src/auth/auth.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Request, Post, UseGuards } from '@nestjs/common';
+import { Controller, Request, Post, UseGuards, Get } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 import { AuthService } from './auth.service';
 
@@ -10,5 +10,11 @@ export class AuthController {
     @Post('/login')
     async login(@Request() req) {
         return this.authService.login(req.user);
+    }
+    
+    @UseGuards(AuthGuard('jwt'))
+    @Get('secretURL')
+    secret(@Request() req) {
+        return req.user;
     }
 }
--- server/src/auth/auth.module.ts
+++ server/src/auth/auth.module.ts
@@ -3,6 +3,7 @@ import { AuthService } from './auth.service';
 import { DatabaseModule } from '../database/user.module';
 import { PassportModule } from '@nestjs/passport';
 import { LocalStrategy } from './local.strategy';
+import { JwtStrategy } from './jwt.strategy';
 import { AuthController } from './auth.controller';
 import { JwtModule } from '@nestjs/jwt';
 import { jwtConstants } from './constants';
@@ -16,7 +17,8 @@ import { jwtConstants } from './constants';
       signOptions: { expiresIn: '60s' },
     }),
   ],
-  providers: [AuthService, LocalStrategy],
-  controllers: [AuthController]
+  providers: [AuthService, LocalStrategy, JwtStrategy],
+  controllers: [AuthController],
+  exports: [AuthService],
 })
 export class AuthModule {}
