3a6b4ecac2ff5f82f55eea2520339fdc322367f9
MSupial <petertconti@gmail.com>
2020-08-17 17:03:28 -0400

    hide login and register nav links if user logged in

--- app/src/app.tsx
+++ app/src/app.tsx
@@ -1,71 +1,78 @@
-import * as React from 'react';
-import { authService } from './auth.service';
-import './index.css';
-import { BrowserRouter as Router, Route, Link, Switch, useHistory } from 'react-router-dom';
-import LoginForm from './login';
-import Register from './register';
-import Test from './test';
-import { withRouter } from 'react-router';
-import PrivateRoute from './components/protected-route';
+import * as React from "react";
+import { authService } from "./auth.service";
+import "./index.css";
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+  Switch,
+  useHistory,
+} from "react-router-dom";
+import LoginForm from "./login";
+import Register from "./register";
+import Test from "./test";
+import { withRouter } from "react-router";
+import PrivateRoute from "./components/protected-route";
 
 interface appState {
-    currentUser: object | null
+  currentUser: object | null;
 }
 export default class App extends React.Component<{}, appState> {
-    constructor(props: any) {
-        super(props);
+  constructor(props: any) {
+    super(props);
 
-        this.state = {
-            currentUser: null
-        };
-    }
-    
+    this.state = {
+      currentUser: null,
+    };
+  }
 
-    componentDidMount() {
-        authService.currentUser.subscribe(x => this.setState({ currentUser: x }));
-    }
+  componentDidMount() {
+    authService.currentUser.subscribe((x) => this.setState({ currentUser: x }));
+  }
 
-    // logout() {
-    //     authService.logout();
-    //     //this.props.history.push('/login');
+  // logout() {
+  //     authService.logout();
+  //     //this.props.history.push('/login');
+  // }
+
+  render() {
+    // /* every time rendering happens, is when the check to see if user
+    //  * is logged in or not should occur */
+    // console.log(this.state.currentUser);
+    // if (!this.state.currentUser) {
+    //         console.log('bitch i should be showing login');
+    //     return (
+    //         <div>
+    //             <Login />
+    //         </div>
+    //     )
     // }
-
-    render() {
-        // /* every time rendering happens, is when the check to see if user
-        //  * is logged in or not should occur */
-        // console.log(this.state.currentUser);
-        // if (!this.state.currentUser) {
-        //         console.log('bitch i should be showing login');
-        //     return (
-        //         <div>
-        //             <Login />
-        //         </div>
-        //     )
-        // }
-        // else {
-        //     return (
-        //         <div>
-        //             <Lookup />
-        //             <Logout />
-        //         </div>
-        //     );
-        // }
-        return(
-            <Router>
-                <div>
-                    <nav>
-                        <Link to="/login">Bitch log in</Link>
-                        <Link to="/register">Bitch register</Link>
-                    </nav>
-                    <Switch>
-                        <Route exact path="/login" component={LoginForm} />
-                        <Route exact path="/register" component={Register} />
-                        <PrivateRoute exact path="/test" component={Test} />
-                    </Switch>
-                </div>
-            </Router>
-        )
-    }
+    // else {
+    //     return (
+    //         <div>
+    //             <Lookup />
+    //             <Logout />
+    //         </div>
+    //     );
+    // }
+    return (
+      <Router>
+        <div>
+          <nav>
+            {!this.state.currentUser && <Link to="/login">Bitch log in</Link>}
+            {!this.state.currentUser && (
+              <Link to="/register">Bitch register</Link>
+            )}
+          </nav>
+          <Switch>
+            <Route exact path="/login" component={LoginForm} />
+            <Route exact path="/register" component={Register} />
+            <PrivateRoute exact path="/test" component={Test} />
+          </Switch>
+        </div>
+      </Router>
+    );
+  }
 }
 
 export { App };
