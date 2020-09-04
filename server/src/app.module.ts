33572c485675e8f858430eb9d0f2b8761a518657
contip <contip@oregonstate.edu>
2020-09-04 04:22:07 -0400

    transfer to postgres, need to fix custom queries

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -4,15 +4,27 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from './auth/auth.module';
 import { LookupModule } from './lookup/lookup.module';
 import { ConfigModule } from '@nestjs/config';
+import { UserEntity } from './database/entities/users.entity';
+import { Trades } from './database/entities/trades.entity';
 
+console.log(process.env);
 @Module({
   imports: [
-    TypeOrmModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.POSTGRES_HOST,
+      port: parseInt(process.env.POSTGRES_PORT),
+      username: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
+      database: process.env.POSTGRES_DB,
+      entities: [UserEntity, Trades],
+      synchronize: true,  //dev only
+    }),
     HttpModule,
     DatabaseModule,
     AuthModule,
     LookupModule,
-    ConfigModule.forRoot(),
+    ConfigModule.forRoot({ isGlobal: true }),
   ],
   controllers: [],
   providers: [],
